d23dd17b2118663122000021ed2c77b5de273f3a
Germán Franco <dev.germanfr@gmail.com>
2017-03-26 13:53:31 +0200

    Rewrite using OO and more clear implementation

--- petri.js
+++ petri.js
@@ -1,114 +1,15 @@
 var petri = (function() {
 	var module = {};
 
-	var canvasWidth;
-	var canvasHeight;
 
 	var dotDensity = 2.5;
-	var dotColor = "rgba(255,255,255,.5)";
-	var nDots;
-	var maxSpeed = 16;
-	var maxRadius = 2;
-	var minRadius = 1;
-	var At = 0.016; //60fps
-	var maxDistanceUnion = 100;
 
-	var timeout = null;
-	var where_id = "petri";
-
-	module.start = function (where, width, height, changeBG) {
-
-		where_id = where;
-		var canvas = document.getElementById(where);
-		if(canvas === null) {
-			console.error("Canvas not found. Add onload event listener!");
-		}
-		else {
-			module.stop();
-			canvasWidth = width || window.innerWidth;
-			canvasHeight = height || window.innerHeight;
-			nDots = Math.floor(canvasHeight*canvasWidth/10000*dotDensity);
-			if(changeBG || changeBG === undefined) {
-				setBackground(canvas);
-			}
-			resizeCanvas(canvas);
-			var sup = canvas.getContext("2d");
-
-			canvas.dots = createDots(nDots);
-			var tree = new Quadtree({
-				x: 0,
-				y: 0,
-				width: canvas.width,
-				height: canvas.height,
-			},10,3);
-
-			var anim = function(curr_time) {
-				sup.clearRect(0,0,canvas.width,canvas.height);
-
-				At = (curr_time - anim.last_callback) / 1000;
-				anim.last_callback = curr_time;
-
-				for(var i = 1; i<nDots; i++) {
-					canvas.dots[i].evol(At);
-					canvas.dots[i].draw(sup);
-				}
-				tree.update(canvas.dots);
-
-				for(var i = 0; i<nDots; i++) {
-					canvas.dots[i].unir(sup,tree);
-				}
-				timeout = window.requestAnimationFrame(anim);
-			};
-			anim.last_callback = 0;
-			timeout = window.requestAnimationFrame(anim);
-
-			canvas.addEventListener("mousemove", old_mouse = function(e) {
-				canvas.dots[0].x = e.clientX;
-				canvas.dots[0].y = e.clientY;
-			});
-
-		}
-	}
-
-	module.stop = function() {
-		if(timeout !== null) {
-			cancelAnimationFrame(timeout);
-			timeout = null;
-		}
-	}
-	module.clear = function() {
-		module.stop();
-		var canvas = document.getElementById(where_id).getContext("2d");
-		canvas.clearRect(0,0,canvasWidth,canvasHeight);
-	}
-
-	module.play = function(){
-		if(timeout === null) {
-			module.start(where_id,canvasWidth,canvasHeight,false);
-		}
-	}
-
-	/**
-	* Returns a random number between A and B.
-	* @param {number} A - Minimum value.
-	* @param {number} B - Maximum value.
-	*/
-	function random(A, B) {
-       	return Math.floor(Math.random()*(B-A+1)+A);
-	}
 
 	function resizeCanvas(canvas) {
-		canvas.height = canvasHeight;
-		canvas.width = canvasWidth;
+		canvas.height = canvas.height;
+		canvas.width = canvas.width;
 	}
 
-	function createDots (n) {
-		var arr = [];
-		for(var i = 0; i<n; i++) {
-			arr[i] = new Dot();
-		}
-		return arr;
-	}
 
 	function setBackground(elem) {
 		var colors = ["336699","20C29A","8C20C2","0E193D"];
@@ -133,79 +34,134 @@ var petri = (function() {
 		elem.style.backgroundImage = "linear-gradient(" + degrees + "deg, #" + hex1 + ", #" + hex2 + ")";
 	}
 
-	function Dot() {
-		/*Position*/
-		this.x = random(0,canvasWidth);
-		this.y = random(0,canvasHeight);
-		this.radius = random(minRadius, maxRadius);
-		this.randomSpeed();
 
-		//For quadtree support. Not used
-		this.width = 0;
-		this.height = 0;
+	/**
+	* Returns a random number between A and B, both included.
+	* @param {number} min - Minimum value.
+	* @param {number} max - Maximum value.
+	*/
+	function random(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
 	}
 
-	Dot.prototype.randomSpeed = function() {
-		//var speed = puntos.maxSpeed* (1 - (this.radius-puntos.minRadius)/puntos.maxRadius);
-		var speed = maxSpeed * (this.radius - minRadius/2) / (maxRadius - minRadius/2);
-		var dir = Math.random()*Math.PI*2;
-		this.vx = speed*Math.cos(dir);
-		this.vy = speed*Math.sin(dir);
+	/**
+	* Round module including negative numbers.
+	* Example: -1 % 4 === 3
+	* @param {number} div - Dividend
+	* @param {number} dsor - Divisor
+	*/
+	function round_mod(div, dsor) {
+		return ((div % dsor) + dsor) % dsor;
 	}
 
-	Dot.prototype.draw = function(canvas) {
-		canvas.beginPath();
-		canvas.fillStyle = dotColor;
-		canvas.arc(this.x,this.y,this.radius,0,2*Math.PI);
-		canvas.fill();
+	const TWO_PI = 2 * Math.PI;
+
+	const DOT_MIN_RADIUS = 1;
+	const DOT_MAX_RADIUS = 2;
+	const DOT_MIN_SPEED = 4;
+	const DOT_MAX_SPEED = 12;
+
+	const MAX_DISTANCE_UNION = 100;
+
+	const DRAW_DOT_COLOR = 'rgba(255,255,255,0.75)';
+
+	function Dot(canvas, x, y, radius) {
+		this._init(canvas, x, y, radius);
 	}
 
-	Dot.prototype.evol = function(t) {
-		this.x = (this.x + this.vx*t).mod(canvasWidth);
-		this.y = (this.y + this.vy*t).mod(canvasHeight);
-	}
+	Dot.prototype = {
+		_init: function(canvas, x, y, radius) {
+			this.canvas = canvas;
+			this.context = canvas.getContext('2d');
 
-	Dot.prototype.unir = function(canvas,quadtree) {
-		var elems = quadtree.retrieve({
-			x: this.x - maxDistanceUnion,
-			y: this.y - maxDistanceUnion,
-			width: maxDistanceUnion*2,
-			height: maxDistanceUnion*2
-		});
+			this.x = x;
+			this.y = y;
+			this.radius = radius;
+			this.vx = 0;
+			this.vy = 0;
 
-		var distance;
+			this.adjacents = [];
+			this.painted = false;
+		},
 
-		for(var i = 0; i<elems.length; i++) {
-			distance = this.distance(elems[i]);
-			if(distance < maxDistanceUnion) {
-				canvas.strokeStyle = "rgba(255,255,255," + ((1-distance/maxDistanceUnion)/4) + ")";
-				canvas.beginPath();
-				canvas.moveTo(this.x,this.y);
-				canvas.lineTo(elems[i].x,elems[i].y);
-				canvas.stroke();
+		distance: function(other) {
+			var dx = this.x - other.x,
+			    dy = this.y - other.y;
+			return Math.sqrt(dx * dx + dy * dy);
+		},
+
+		step: function(dt) {
+			this.x = round_mod(this.x + this.vx * dt, canvas.width);
+			this.y = round_mod(this.y + this.vy * dt, canvas.height);
+			this.painted = false;
+		},
+
+		draw: function() {
+			let canvas = this.context;
+			canvas.beginPath();
+			canvas.fillStyle = DRAW_DOT_COLOR;
+			canvas.arc(this.x, this.y, this.radius, 0, TWO_PI);
+			canvas.fill();
+
+			// canvas.beginPath();
+			// canvas.strokeStyle = 'rgba(255,0,0,1)';
+			// canvas.lineWidth = 0.5;
+			// canvas.arc(this.x, this.y, MAX_DISTANCE_UNION, 0, TWO_PI);
+			// canvas.stroke();
+
+			this._draw_unions();
+			this.painted = true;
+		},
+
+		_draw_unions: function() {
+			var neighbor;
+			for(let i = 0; i < this.adjacents.length; ++i) {
+				neighbor = this.adjacents[i];
+				if(!neighbor.painted) {
+					this._draw_union_with(neighbor);
+				}
 			}
-		}
+		},
 
-	}
+		_draw_union_with: function(other) {
+			let canvas = this.context;
+			let opacity = (1 - this.distance(other) / MAX_DISTANCE_UNION); // Max opacity is 0.75, min is 0
+			opacity *= opacity * 0.9;
 
-	Dot.prototype.distance = function(dot)  {
-		var dx = Math.abs(this.x - dot.x),
-			dy = Math.abs(this.y - dot.y);
-		return Math.sqrt(dx*dx + dy*dy);
-	}
-
-	Number.prototype.mod = function(n) {
-	    return ((this % n) + n) % n;
-	};
-
-	Quadtree.prototype.update = function(objs) {
-		this.clear();
-		for(var i=0; i<objs.length; i++) {
-			this.insert(objs[i]);
+			canvas.beginPath();
+			canvas.strokeStyle = 'rgba(255,255,255,' + opacity + ')';
+			canvas.lineWidth = 1;
+			canvas.moveTo(this.x, this.y);
+			canvas.lineTo(other.x, other.y);
+			canvas.stroke();
 		}
 	}
 
+	function RandomDot(canvas) {
+		this._init(canvas);
+	}
 
+	RandomDot.prototype = {
+		__proto__: Dot.prototype,
+
+		_init: function(canvas) {
+			let pos_x = Math.random() * canvas.width;
+			let pos_y = Math.random() * canvas.height;
+			let radius = random(DOT_MIN_RADIUS, DOT_MAX_RADIUS);
+			Dot.prototype._init.call(this, canvas, pos_x, pos_y, radius);
+
+			this._init_movement();
+		},
+
+		_init_movement: function() {
+			// The smaller the slower (because it's supposed to be farther)
+			var speed = DOT_MIN_SPEED + (DOT_MAX_SPEED - DOT_MIN_SPEED) *
+			            (this.radius - DOT_MIN_RADIUS) / (DOT_MAX_RADIUS - DOT_MIN_RADIUS);
+			var direction = Math.random() * TWO_PI;
+			this.vx = speed * Math.cos(direction);
+			this.vy = speed * Math.sin(direction);
+		},
+	}
 
 	function Grid(width, height) {
 		this._init.apply(this, arguments);
@@ -219,33 +175,59 @@ var petri = (function() {
 			this.width = width;
 			this.height = height;
 
-			this.columns = this.rows = 8;
+			this.columns = this.rows = 6;
 
-			this.grid = [];
-			for(var j = 0; j < this.columns; ++j) {
-				this.grid.push([]);
+			this.grid = new Array(this.rows);
+			for(let i = 0; i < this.rows; ++i) {
+				this.grid[i] = new Array(this.columns);
+				for(let j = 0; j < this.columns; ++j) {
+					this.grid[i][j] = [];
+				}
 			}
 		},
 
 		insert: function(point) {
-			var col = point.x / this.width * this.columns,
-			    row = point.y / this.height * this.rows;
-
-			var section = this.grid[row][col];
+			var section = this.get_section(point.x, point.y);
 			if(section.indexOf(point) < 0) {
 				section.push(point);
 			}
 		},
 
+		update_point: function(old_x, old_y, point) {
+			let old_section = this.get_section(old_x, old_y);
+			let pos = old_section.indexOf(point);
+
+			if(pos < 0)
+				return;
+
+			old_section.splice(pos, 1);
+
+			let new_section = this.get_section(point.x, point.y);
+			new_section.push(point);
+		},
+
+		get_section: function(x, y) {
+			return this.grid[this.get_row(y)][this.get_column(x)];
+		},
+
+		get_column: function(x) {
+			return Math.floor(x * this.columns / this.width);
+		},
+
+		get_row: function(y) {
+			return Math.floor(y * this.rows / this.height);
+		},
+
 		visible_points: function(point, radius) {
 			var visible = [];
-			var init_col = Math.max((point.x - radius) / this.width * this.columns, 0),
-			     end_col = Math.min((point.x + radius) / this.width * this.columns, this.columns - 1);
-			var init_row = Math.max((point.y - radius) / this.height * this.rows, 0),
-			     end_row = Math.min((point.y + radius) / this.height * this.rows, this.rows - 1);
+			var init_col = Math.max(this.get_column(point.x - radius), 0),
+			     end_col = Math.min(this.get_column(point.x + radius) + 1, this.columns);
+			var init_row = Math.max(this.get_row(point.y - radius), 0),
+			     end_row = Math.min(this.get_row(point.y + radius) + 1, this.rows);
 
-			for(var i = init_row; i < end_row; ++i) {
-				for(var j = init_col; i < end_col; ++j) {
+
+			for(let i = init_row; i < end_row; ++i) {
+				for(let j = init_col; j < end_col; ++j) {
 					this.grid[i][j].forEach(function(other) {
 						if(point.distance(other) <= radius) {
 							visible.push(other);
@@ -256,6 +238,83 @@ var petri = (function() {
 			return visible;
 		}
 	}
+
+	function Petri(canvas, n_points) {
+		this._init(canvas, n_points);
+	}
+
+	Petri.prototype = {
+		_init: function(canvas, n_points) {
+			this.canvas = canvas;
+			this.context = canvas.getContext('2d');
+			this.width = canvas.width;
+			this.height = canvas.height;
+
+			this.surface = new Grid(this.width, this.height);
+
+			this.points = new Array(n_points);
+			for(let i = 0; i < n_points; ++i) {
+				this.points[i] = new RandomDot(this.canvas);
+				this.surface.insert(this.points[i]);
+			}
+		},
+
+		_update_graph: function () {
+			var point;
+			for(let i = 0; i < this.points.length; ++i) {
+				point = this.points[i];
+				point.adjacents = this.surface.visible_points(point, MAX_DISTANCE_UNION);
+			}
+		},
+
+		start: function() {
+			if(this.run) return;
+
+			this.run = true;
+			this.time = performance.now();
+			window.requestAnimationFrame(this._step_frame.bind(this));
+		},
+
+		stop: function () {
+			this.run = false;
+		},
+
+		_step_frame: function(current_time) {
+			let time_diff = (current_time - this.time) / 1000;
+			this.step(time_diff);
+			this.draw();
+
+			this.time = current_time;
+
+			if(this.run) {
+				window.requestAnimationFrame(this._step_frame.bind(this));
+			}
+		},
+
+		step: function(dt) {
+			var point;
+			var old_x, old_y;
+			for(let i = 0; i < this.points.length; ++i) {
+				point = this.points[i];
+				old_x = point.x; old_y = point.y;
+				point.step(dt);
+				this.surface.update_point(old_x, old_y, point);
+			}
+			this._update_graph();
+		},
+
+		draw: function() {
+			var point;
+
+			this.context.clearRect(0 , 0, this.canvas.width, this.canvas.height);
+			for(let i = 0; i < this.points.length; ++i) {
+				point = this.points[i];
+				point.draw();
+			}
+		}
+	}
+
+	module.Petri = Petri;
 
 	return module;
 })();
